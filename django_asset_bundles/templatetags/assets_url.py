7431f38151ea45c459fcc56b89cef332d0557061
Benjamin Wohlwend <piquadrat@gmail.com>
2015-11-07 16:57:21 +0100

    implemented asset template tag and as_asset_list filter
    with input from @vad and @jaylett

--- django_asset_bundles/templatetags/assets_url.py
+++ django_asset_bundles/templatetags/assets_url.py
@@ -1,78 +1,41 @@
-import json
-from django.conf import settings
+import warnings
+
 from django import template
 
-_MANIFEST_CACHE = False
-
-class UnknownBundleError(Exception):
-    pass
-
-def read_manifest():
-    """Returns the manifest. Reads it from settings.ASSETS_MANIFEST_FILE if
-    necessary"""
-    with open(settings.ASSETS_MANIFEST_FILE, 'r') as manifest_fd:
-        return json.loads(manifest_fd.read())
-
-
-def get_asset_urls(bundle_name):
-    """Gets the urls associated with a bundle from the manifest
-    @param bundle_name: The bundle name to look for in the manifest
-    @rtype : list
-    """
-    global _MANIFEST_CACHE
-
-    if getattr(settings, 'ASSETS_CACHE', False) and _MANIFEST_CACHE:
-        manifest = _MANIFEST_CACHE
-    else:
-        manifest = read_manifest()
-        _MANIFEST_CACHE = manifest
-
-    try:
-        urls = manifest[bundle_name]
-    except KeyError:
-        raise UnknownBundleError("Bundle '{}' was not found in manifest at {}".format(
-            bundle_name, settings.ASSETS_MANIFEST_FILE
-        ))
-    url_base = getattr(settings, 'ASSETS_URL_BASE', '')
-    return [url_base + u for u in urls]
-
+from ..utils import get_asset_url
 
 register = template.Library()
 
-def do_asset(parser, token):
-    tokens = token.split_contents()
-    tag_name = tokens[0]
-    args = tokens[1:]
 
-    in_quotes = all(
-        (n[0] == n[-1] and n[0] in ('"', "'")) for n in args
-    )
+@register.simple_tag
+def asset(logical_path):
+    """
+    Usage:
+        <script src="{% asset "logical_path" %}"></script>
 
-    if not in_quotes:
-        raise template.TemplateSyntaxError, "%r tag's argument should be in quotes" % tag_name
-
-    # Get rid of quotes
-    bundle_names = [n[1:-1] for n in args]
-
-    nodelist = parser.parse(('end_include_bundle',))
-    parser.delete_first_token()
-
-    return AssetNode(bundle_names, nodelist)
+    :param logical_path:
+    :return: an asset path
+    """
+    asset = get_asset_url(logical_path)
+    if not isinstance(asset, basestring):
+        warnings.warn("Please use the as_asset filter if your "
+                      "pipeline returns lists")
+    return asset
 
 
-class AssetNode(template.Node):
-    def __init__(self, bundle_names, nodelist):
-        self.nodelist = nodelist
-        self.bundle_names = bundle_names
+@register.filter
+def as_asset_list(logical_path):
+    """
+    Usage:
 
-    def render(self, context):
-        output = ""
-        for bundle in self.bundle_names:
-            for url in get_asset_urls(bundle):
-                context.push()
-                context['ASSET_URL'] = url
-                output += self.nodelist.render(context)
-                context.pop()
-        return output
+        {% for file in "logical_path"|as_asset_list %}
+            <script src="{{ file }}"></script>
+        {% endfor %}
 
-register.tag('include_bundle', do_asset)
+    :param logical_path:
+    :return: list of asset paths
+    """
+    assets = get_asset_url(logical_path)
+    if isinstance(assets, basestring):
+        return [assets]
+    return assets
