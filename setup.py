89ac37f68ba140890ee09d959cd78babf2e35774
Davide Setti <davide.setti@gmail.com>
2015-11-07 17:26:59 +0100

    remove pytest dependency. Fix the setup.py shebang

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
+#  -*- coding: utf-8 -*-
 
 try:
     from setuptools import setup
@@ -11,12 +11,11 @@ with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    'django'
+    'django',
 ]
 
 test_requirements = [
-    'pytest',
-    'pytest-django',
+    'Jinja2',
 ]
 
 setup(
